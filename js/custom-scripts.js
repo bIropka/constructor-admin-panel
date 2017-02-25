1dadb9e975eed28bd97590d17052bebce15fb340
bIropka <biropka@gmail.com>
2017-02-25 17:11:04 +0200

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -14,6 +14,8 @@ $(document).ready(function() {
 
     $('.width-and-colors').mCustomScrollbar();
 
+    $('.general-settings').mCustomScrollbar();
+
     $('.color-input').click(function() {
 
         if($(this).parent().position().top > $(this).parents('form').height() - 260) {
@@ -40,47 +42,53 @@ $(document).ready(function() {
 
     $('.nav li').click(function() {
 
-        switch ($(this).attr('class')) {
+        if ($(this).hasClass('btn-show-result')) {
 
-            case 'btn-show-result':
+        } else if ($(this).hasClass('btn-add-element')) {
 
-                break;
+        } else if ($(this).hasClass('btn-edit-text')) {
 
-            case 'btn-add-element':
+        } else if ($(this).hasClass('btn-edit-styles')) {
 
-                break;
+        } else if ($(this).hasClass('btn-choose-grid')) {
 
-            case 'btn-edit-text':
+        } else if ($(this).hasClass('btn-edit-background-width')) {
 
-                break;
-
-            case 'btn-edit-styles':
-
-                break;
-
-            case 'btn-choose-grid':
-
-                break;
-
-            case 'btn-edit-background-width':
-                $('.general-settings').removeClass('active');
+            if($(this).hasClass('active') || !$('.btn-edit-info').hasClass('active')) {
+                $('.width-and-colors').toggleClass('active');
+            } else {
                 setTimeout(function() {
                     $('.width-and-colors').toggleClass('active');
-                }, 600);
-                break;
+                }, 500);
+            }
+            $('.btn-edit-info').removeClass('active');
+            $(this).toggleClass('active');
+            $('.general-settings').removeClass('active');
 
-            case 'btn-edit-info':
-                $('.width-and-colors').removeClass('active');
+        } else if ($(this).hasClass('btn-edit-info')) {
+
+            if($(this).hasClass('active') || !$('.btn-edit-background-width').hasClass('active')) {
+                $('.general-settings').toggleClass('active');
+            } else {
                 setTimeout(function() {
                     $('.general-settings').toggleClass('active');
-                }, 600);
-                break;
+                }, 500);
+            }
+            $('.btn-edit-background-width').removeClass('active');
+            $(this).toggleClass('active');
+            $('.width-and-colors').removeClass('active');
 
-            case 'btn-save-template':
 
-                break;
+        } else if ($(this).hasClass('btn-save-template')) {
 
         }
+
+    });
+
+    $('input[type="file"]').change(function() {
+
+        var fileName = $(this).val().split('\\');
+        $('<span class="file-name">' + fileName[fileName.length - 1] + '</span>').insertAfter($(this));
 
     });
 
